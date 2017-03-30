991d0ef7754b7dbdf9386642db14eb6da32e9a8e
kaushal kishore <kaushalkishorersmt@gmail.com>
2017-03-31 02:29:02 +0530

    Add user and task model for initial commit.

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+
+  root to: "tasks#index"
+
+  resources :tasks
+end
